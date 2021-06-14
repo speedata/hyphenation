70df5f81ea1b37177173ec7f82513f8eda7c80c3
Patrick Gundlach <gundlach@speedata.de>
2021-06-14 17:04:10 +0200

    Hyphenation debugging, more test cases

--- hyphenate_test.go
+++ hyphenate_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-const patterns string = `1co
+const patternsEN string = `1co
 4m1p
 pu2t
 5pute
@@ -22,8 +22,47 @@ s1si
 o2n
 `
 
+const patternsDE string = `
+.sc4h4
+1de
+1fa
+1fe
+1ne
+1sc
+1se
+1ße
+1ta
+2ä3s2e
+2hn
+2hr
+2if
+4ahr
+4f1f
+4l1l
+4n1g
+4n1n
+4r1t
+4rn
+5fahrt
+6r1s
+c2h
+g2r4
+gl2
+h2ü
+h3ner
+hn2e
+hühne4
+ng2läs
+ö1ß
+rn3g2
+s1t
+s2ta
+s4er.
+üh3ne
+`
+
 func TestHyphenate(t *testing.T) {
-	r := strings.NewReader(patterns)
+	r := strings.NewReader(patternsEN)
 	l, err := New(r)
 	if err != nil {
 		t.Error(err)
@@ -36,6 +75,38 @@ func TestHyphenate(t *testing.T) {
 		{"possession", []int{3, 6}},
 		{"Computer", []int{3, 6}},
 		{"a", []int{}},
+	}
+
+	for _, entry := range data {
+		h := l.Hyphenate(entry.word)
+		if len(h) != len(entry.breakpoints) {
+			t.Errorf("Hyphenate(%s), len = %d, want %d", entry.word, len(h), len(entry.breakpoints))
+		}
+
+		for i, v := range h {
+			if bp := entry.breakpoints[i]; bp != v {
+				t.Errorf("Hyphenate(%s), breakpoint[%d] = %d, want %d", entry.word, i, bp, v)
+			}
+		}
+	}
+}
+
+func TestHyphenateDE(t *testing.T) {
+	r := strings.NewReader(patternsDE)
+	l, err := New(r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	data := []struct {
+		word        string
+		breakpoints []int
+	}{
+		{"größer", []int{3}},
+		{"Schiffahrt", []int{5, 9}},
+		{"Hühnerstall", []int{3, 6, 10}},
+		{"ferngläser", []int{4, 7}},
+		{"denn", []int{3}},
 	}
 
 	for _, entry := range data {
--- hyphenation.go
+++ hyphenation.go
@@ -13,6 +13,7 @@ package hyphenation
 import (
 	"bufio"
 	"io"
+	"strings"
 	"unicode"
 )
 
@@ -75,6 +76,117 @@ func New(r io.Reader) (*Lang, error) {
 	return l, s.Err()
 }
 
+type patternposition struct {
+	startpos int
+	wordpart []rune
+	pattern  []byte
+}
+
+func (l *Lang) doHyphenate(rword []rune) []patternposition {
+	var patterninfo []patternposition
+	var startpos int
+	var wordpart []rune
+	// generate all possible substrings for the word
+	for j := 1; j < len(rword); j++ {
+		startpos = j - 1
+		// if rword[0] == '.' {
+		// 	startpos = startpos + 1
+		// }
+		for i := j + 1; i <= len(rword); i++ {
+			wordpart = rword[startpos:i]
+
+			// if there is a pattern for this substring
+			if pattern, ok := l.patterns[string(wordpart)]; ok {
+				patterninfo = append(patterninfo, patternposition{
+					startpos: startpos,
+					wordpart: wordpart,
+					pattern:  pattern,
+				})
+			}
+		}
+	}
+	return patterninfo
+}
+
+// DebugHyphenate returns a multi-line string with information about the patterns used and the priorities.
+func (l *Lang) DebugHyphenate(word string) string {
+	var rword []rune
+	for _, letter := range "." + word + "." {
+		rword = append(rword, unicode.ToLower(letter))
+	}
+	breakpoints := l.doHyphenate(rword)
+	maxPrio := make([]byte, len(rword)-1)
+	var b strings.Builder
+	b.WriteString("   ")
+	for _, v := range rword {
+		b.WriteRune(v)
+		b.WriteString("   ")
+	}
+	b.WriteByte(10)
+
+	for _, bp := range breakpoints {
+		startpos := bp.startpos
+		sub := 1
+
+		if bp.wordpart[0] == '.' {
+			sub = 0
+		}
+		for i := 0; i < len(bp.pattern); i++ {
+			if bp.pattern[i] > maxPrio[startpos-sub+i] {
+				maxPrio[startpos-sub+i] = bp.pattern[i]
+			}
+		}
+
+		b.WriteString("    ")
+		for i := 0; i < bp.startpos-sub; i++ {
+			b.WriteString(" |  ")
+		}
+		for i := 0; i < len(bp.pattern); i++ {
+			b.WriteRune(' ')
+			b.WriteRune('0' + rune(bp.pattern[i]))
+			b.WriteRune(' ')
+			b.WriteRune(' ')
+		}
+		for i := bp.startpos + len(bp.pattern) - sub; i < len(rword); i++ {
+			b.WriteString(" |  ")
+		}
+		b.WriteRune(' ')
+		for i := 0; i < len(bp.wordpart); i++ {
+			if bp.pattern[i] > 0 {
+				b.WriteRune(rune(bp.pattern[i]) + '0')
+			}
+			b.WriteRune(bp.wordpart[i])
+		}
+		lastprio := bp.pattern[len(bp.pattern)-1]
+		if lastprio > 0 {
+			b.WriteRune(rune(lastprio) + '0')
+		}
+		b.WriteByte(10)
+	}
+	b.WriteString("max: ")
+	for i := 0; i < len(maxPrio); i++ {
+		b.WriteRune('0' + rune(maxPrio[i]))
+		b.WriteString("   ")
+	}
+	b.WriteByte(10)
+
+	b.WriteString("final: ")
+	for i := 1; i < len(rword)-1; i++ {
+		b.WriteRune(rword[i])
+		b.WriteRune(' ')
+		if maxPrio[i]%2 == 0 {
+			b.WriteRune(' ')
+		} else {
+			b.WriteRune('-')
+		}
+		b.WriteRune(' ')
+
+	}
+
+	b.WriteByte(10)
+	return b.String()
+}
+
 // Hyphenate returns an array of int with resulting break points.
 // For example the word “developers” with English (US) hyphenation
 // patterns could return [2 5 7 9] which means de-vel-op-er-s
@@ -83,38 +195,28 @@ func (l *Lang) Hyphenate(word string) []int {
 	for _, letter := range "." + word + "." {
 		rword = append(rword, unicode.ToLower(letter))
 	}
-	var startpos int
-	var wordpart []rune
-	// maxPrio[i] contains the maximum priority before the letter i
+
+	breakpoints := l.doHyphenate(rword)
 	maxPrio := make([]byte, len(rword)-1)
 
-	// generate all possible substrings for the word
-	for j := 1; j < len(rword); j++ {
-		startpos = j - 1
-		if rword[0] == '.' {
-			startpos = startpos + 1
+	for _, bp := range breakpoints {
+		// pattern is a byte slice such as [0 0 1 0]
+		// associated with the word part, "dev" for example
+		// the pattern in the hyphenation file would be
+		// de1v or similar 0d0e1v0
+		// when the pattern contains a dot at the beginning, this
+		// marks the start of the word. Therefore there is no
+		// priority before the .
+		startpos := bp.startpos
+		if bp.wordpart[0] == '.' {
+			startpos++
 		}
-		for i := j + 1; i <= len(rword); i++ {
-			wordpart = rword[startpos:i]
-
-			// if there is a pattern for this substring
-			if pattern, ok := l.patterns[string(wordpart)]; ok {
-				// pattern is a byte slice such as [0 0 1 0]
-				// associated with the word part, "dev" for example
-				// the pattern in the hyphenation file would be
-				// de1v or similar 0d0e1v0
-				// when the pattern contains a dot at the beginning, this
-				// marks the start of the word. Therefore there is no
-				// priority before the .
-				for i := 0; i < len(pattern); i++ {
-					if pattern[i] > maxPrio[startpos-1+i] {
-						maxPrio[startpos-1+i] = pattern[i]
-					}
-				}
+		for i := 0; i < len(bp.pattern); i++ {
+			if bp.pattern[i] > maxPrio[startpos-1+i] {
+				maxPrio[startpos-1+i] = bp.pattern[i]
 			}
 		}
 	}
-
 	// the odd entries in maxPrio are valid break points
 	var positions []int
 	for i := 1; i < len(maxPrio); i++ {
